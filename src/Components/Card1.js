35366de937b2e25e37fe8dbfbb9712d0ad157159
Manikandan610 <manikandan.jn@fibonalabs.com>
2022-07-01 18:54:24 +0530

    Eight Problems Done

--- src/Components/Card1.js
+++ src/Components/Card1.js
@@ -1,29 +1,25 @@
 import React, {useState} from "react";
-import '../App.css';
+//import '../App.css';
+import '../Asset/Style.css';
 //import Card2 from './Card2';
 
 function Card1(){
-    // function changeCard1Color(){
-    //     var color="red";
-    //     var el=document.getElementById('card2');
-    //     el.style.backgroundColor=color;
-    // }
-    // function changeCard2Color(){
-    //     var color="gray";
-    //     var el=document.getElementById('card1');
-    //     el.style.backgroundColor=color;
-    // }
-    const [active,changeColor] = useState(true);
+    
+    const [isActive,card1Color] = useState(true);
+     const [active,card2Color] = useState(true);
     function changeCard1Color(){
-        changeColor(current=>!current);
+        card1Color(current=>!current);
+    }
+    function changeCard2Color(){
+        card2Color(current=>!current);
     }
     return(      
         <div className="Container">
-            <div className={active ? 'Card1' : 'Card1-change'} onClick={changeCard1Color} id="card1">
-                <p className={active ? 'Card-p' : 'Card-p1'}>Click Card1 it will be changed card2 backgroundColor.</p>
+            <div className={active ? 'Card1' : 'Card1-change'} onClick={changeCard1Color}>
+                <p className={active ? 'Card-p1' : 'Card-p'}>Click Card1 it will be changed card2 backgroundColor.</p>
             </div>
-            <div className={active ? 'Card1-change' : 'Card1'} id="card2" onClick={changeCard1Color}>
-                <p className={active ? 'Card-p1' : 'Card-p'}>Click card2 it will be changed Card1 backgroundColor.</p>
+            <div className={isActive ? 'Card2-change' : 'Card2'} onClick={changeCard2Color}>
+                <p className={isActive ? 'Card-p' : 'Card-p1'}>Click card2 it will be changed Card1 backgroundColor.</p>
          </div>
      </div>    
            
